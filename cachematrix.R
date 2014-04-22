881735678429618e86ac29cd376ed73a34df0d82
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-04-22 10:09:22 -0400

    Initial Commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  minv <- NULL
+  set <- function(y) {
+    x <<- y
+    minv <<- NULL
+  }
+  get <- function() x
+  setminv <- function(solve) minv <<- solve
+  getminv <- function() minv
+  list(set = set, get = get,
+       setminv = setminv,
+       getminv = getminv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  minv <- x$getminv()
+  if(!is.null(minv)) {
+    message("getting cached data")
+    return(minv)
+  }
+  data <- x$get()
+  minv <- mean(data, ...)
+  x$setminv(minv)
+  minv
         ## Return a matrix that is the inverse of 'x'
 }
